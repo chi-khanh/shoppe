ac11c0ba6f08542e9ef79570e54d41a358acc5da
phongho <phonghophamminh@gmail.com>
2022-08-03 14:43:48 +0700

    implemenet change mode class

--- features/script.js
+++ features/script.js
@@ -1,5 +1,5 @@
 // variables
-const BASE_URL = "/Volumes/ChiKhanh/work/shoppe";
+const BASE_URL = "/Users/hophong/Desktop/khanh/shoppe";
 const account = {
   dashboard: `/${BASE_URL}/pages/account/dashboard/index.html`,
 };
@@ -21,16 +21,22 @@ const onLoad = () => {
 window.addEventListener(
   "message",
   (e) => {
-    const { header, footer } = JSON.parse(e.data) || {};
+    const { header } = JSON.parse(e.data) || {};
+
+    // mode
+    if (header?.mode) {
+      if (header.mode === "light") {
+        document.body.classList.remove("dark-mode");
+        document.body.classList.add("light-mode");
+      } else {
+        document.body.classList.add("dark-mode");
+        document.body.classList.remove("light-mode");
+      }
+    }
 
     // nagigate by header
     if (header?.navigate) {
       window.location.href = header.navigate.url;
-    }
-
-    // nagigate by tablist
-    if (tablist?.navigate) {
-      window.location.href = tablist.navigate.url;
     }
   },
   false
